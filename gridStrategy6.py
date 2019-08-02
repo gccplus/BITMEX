1b925ff4c67e221a3dfa15081f0cab6bf0de335e
Nynia <jokeforce@gmail.com>
2019-08-02 17:29:04 +0800

    debug

--- gridStrategy6.py
+++ gridStrategy6.py
@@ -28,8 +28,8 @@ class GridStrategy:
     def __init__(self):
         self.logger = setup_logger()
         test = False
-        api_key = 'kVfKITnQdJEzEC2sKYlVr9mM'
-        api_secret = 'joccPUd5_DwOd3CDL1lSq_prKDxxM6oRQCmu7aALcw_6KWCi'
+        self.api_key = 'kVfKITnQdJEzEC2sKYlVr9mM'
+        self.api_secret = 'joccPUd5_DwOd3CDL1lSq_prKDxxM6oRQCmu7aALcw_6KWCi'
         test_url = 'https://testnet.bitmex.com/api/v1'
         product_url = 'https://www.bitmex.com/api/v1'
         if test:
