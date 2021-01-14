36cfdb714a4b0fbe01bbcbe34b4c2ac15b3fa457
Nynia <jokeforce@gmail.com>
2021-01-14 15:30:42 +0800

    更新key

--- notify.py
+++ notify.py
@@ -17,8 +17,8 @@ if __name__ == "__main__":
     setup_logger()
     contract_name = 'XBTUSD'
     test = False
-    api_key = 'RbKv_8cp9-EM5sPKhM2-tcIh'
-    api_secret = '6VEYWM7x6Cg5Uo3iTNAgD997tvgYT4711eXqTXgU6dj7cCoB'
+    api_key = 'zB5xpkIGQ6MLnJGLf86yz4-t'
+    api_secret = 'I_imevnlDZOHe-eiOGmsrOsWJnimKN9iEk7FYYt2r3VvS69x'
     test_url = 'https://testnet.bitmex.com/api/v1'
     product_url = 'https://www.bitmex.com/api/v1'
     cli = bitmex(test=test, api_key=api_key, api_secret=api_secret)
